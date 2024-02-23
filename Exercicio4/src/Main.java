0925f4771da5bd4464a1082420f1e1380259b227
Bagrielzin <89031177+Bagrielzin@users.noreply.github.com>
2024-02-23 14:38:45 -0300

    Add files via upload

---
+++ Exercicio4/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o número de alunos: ");
+        int nAlunos = entrada.nextInt();
+
+        switch (nAlunos){
+            case 10:
+                System.out.println("A sala utilizada será a I-16");
+                break;
+
+            case 20:
+                System.out.println("A sala utilizada será a I-16");
+                break;
+
+            case 30:
+                System.out.println("A sala utilizada será a I-22");
+                break;
+
+            default:
+                System.out.println("Número de alunos inválido");
+                break;
+        }
+    }
+}
