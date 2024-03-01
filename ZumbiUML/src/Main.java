754aabd9bffcf489c2b8400a965bf9f9c1cf625b
Bagrielzin <89031177+Bagrielzin@users.noreply.github.com>
2024-03-01 15:50:17 -0300

    Add files via upload

--- ZumbiUML/src/Main.java
+++ ZumbiUML/src/Main.java
@@ -1,23 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
         Zumbi z1 = new Zumbi();
         Zumbi z2 = new Zumbi();
+        double quantia;
 
         z1.nome = "Dudu";
-        z1.vida = 200.0;
+        z1.vida = 300.0;
         z2.nome = "Schuller";
-        z2.vida = 50;
+        z2.vida = 200;
 
-        if(z1.transfereVida(z1, z2.vida, z2)){
+        System.out.print("Quantia de vida a ser transferida: ");
+        quantia = entrada.nextDouble();
+
+        if(z1.transfereVida(z1, quantia, z2)){
             System.out.println("Transferência de vida feita com sucesso!!");
         }
 
         else{
-            System.out.println("Não foi possível transerir vida pois o outro zumbi já estava morto.");
+            System.out.println("Não foi possível transerir vida.");
         }
 
         System.out.println("Vida do zumbi " + z1.nome + " : " + z1.mostraVida());
         System.out.println("Vida do zumbi " + z2.nome + " : " + z2.mostraVida());
 
+        entrada.close();
     }
 }
